d051f53cf5dff359828c7ee114ca4f89dd1c1e37
0xhatsume <0xhatsume@gmail.com>
2022-08-10 19:25:09 +0800

    feat: add a custom user-controlled erc20 factory

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -2,4 +2,28 @@ import { task } from "hardhat/config";
 import {utils, Contract, BytesLike} from "ethers";
 import { PassThrough } from "stream";
 
-task("placeholder")
+task("erc20Factory", "Deploy a standard ERC20 Token.")
+.addParam("name", "Name of ERC20 Token")
+.addParam("symbol", "Symbol of ERC20 Token")
+.setAction(async (taskArgs, hre) => {
+  //const signers = await ethers.getSigners();
+
+    const ERC20Factory = await hre.ethers.getContractFactory("ERC20");
+    const ERC20 = await ERC20Factory.deploy(
+        taskArgs.name,
+        taskArgs.symbol
+    );
+
+    await ERC20.deployed();
+    console.log(`ERC20 ${taskArgs.name, taskArgs.symbol} deployed to: ${ERC20.address}`);
+});
+
+task("deploy-cake-token")
+.setAction(async(taskArgs, hre)=>{
+    const caketoken = await hre.ethers.getContractFactory("CakeToken").then(
+                async(c)=>{
+                return await c.deploy();
+                }
+    )
+    console.log(`Cake Token deployed at: ${caketoken.address}`)
+})
